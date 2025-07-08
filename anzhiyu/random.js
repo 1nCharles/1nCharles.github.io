6c7abe32e35c492ed00ea7f4e627f0967056abc4
Britney <2855057900@qq.com>
2025-07-08 22:03:24 +0800

    Site updated: 2025-07-08 22:03:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/26/hgame/","2025/02/26/hgame week2/","2025/07/08/pixel6 aosp14编译 升级内核/","2025/03/13/lg v30 lineage os 编译/","2025/03/09/GHCTF WP/","2025/02/26/VNCTF WP/"];function toRandomPost(){
+var posts=["2025/02/26/VNCTF WP/","2025/03/09/GHCTF WP/","2025/02/26/hgame/","2025/02/26/hgame week2/","2025/03/13/lg v30 lineage os 编译/","2025/07/08/pixel6 aosp14编译 升级内核/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
